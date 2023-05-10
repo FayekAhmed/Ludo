16aa72b1e2e744c14a338c15c25720d5829b92d2
Fayek Ahmed <fayekahmed2005@icloud.com>
2023-05-10 16:34:18 -0400

    Update Board.java

--- Assets/Ludo Game/Board.java
+++ Assets/Ludo Game/Board.java
@@ -1,11 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Board extends Pieces {
     public Board() {
 
     }
 
-    public void addBoard() {
-        Image board = Toolkit.getDefaultToolkit().getImage("");
-        g.drawImage(board, 300, 300, this);
-        g.finalize();
-    }
+    public void addBoard(Graphics g) {
+        int x = 600;
+        int y = 300;
+        //outline 1
+        g.setColor(Color.white);
+        g.drawRect(x, y, x + 1900, y + 1900);
+
+        //outline 2
+        g.drawRect(x + 100, y + 100, x + 1800, y + 1800);
+
+        //box1
+        x = 700;
+        y = 400;
+        g.setColor(Color.red);
+        g.drawRect(x, y, x + 600, y + 600);
+
+        //box 2
+        y = 1300;
+        g.setColor(Color.blue);
+        g.drawRect(x, y, x + 600, y + 600);
+
+        //box 3
+        
 }
